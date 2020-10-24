683540720a9df58aa21bc4149ca572c698bdd703
Ross McQuarrie <ross.mcquarrie12@gmail.com>
2020-10-24 11:57:18 +0100

    glide fixes and using external package

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 func main() {
 	fmt.Println("Hello Front End users")
+
+	data := [][]string{
+
+		[]string{"Alfred", "15", "10/20", "(10.32, 56.21, 30.25)"},
+
+		[]string{"Beelzebub", "30", "30/50", "(1,1,1)"},
+
+		[]string{"Hortense", "21", "80/80", "(1,1,1)"},
+
+		[]string{"Pokey", "8", "30/40", "(1,1,1)"},
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+
+	table.SetHeader([]string{"NPC", "Speed", "Power", "Location"})
+
+	table.AppendBulk(data)
+
+	table.Render()
+
+	fmt.Println("Hello Again Front End users")
 }
